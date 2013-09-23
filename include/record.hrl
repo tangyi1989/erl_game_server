92dc006fe9404e24d7ef6d62b53ea4b2a87b91ca
root <root@localhost.localdomain>
2013-09-23 16:32:21 +0800

    更改文件路径

--- include/record.hrl
+++
@@ -1,8 +0,0 @@
-
-
-%%###########ETS##################
--record(ets_online, {
-        id = 0,
-        nickname = none,
-        pid = 0
-    }).
--- tests/test_client.erl
+++
@@ -1,9 +0,0 @@
-
--module(test_client).
-
--export([test/0]).
-
-test() ->
-    {ok, _} = client_sup:start_link(),
-    {ok, _} = supervisor:start_child(client_sup, []),
-    ok.
